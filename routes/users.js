6ac768bad69f5691d4d997f7d58abfe61e708784
farteaga8912 <18072082+frarteaga@users.noreply.github.com>
2019-09-29 12:04:57 -0400

    list users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var users = [
+  {
+    'Name': 'user 1', 'Age': 27
+  },
+  {
+    'Name': 'user 2', 'Age': 25
+  }
+];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send(users);
 });
 
 module.exports = router;
